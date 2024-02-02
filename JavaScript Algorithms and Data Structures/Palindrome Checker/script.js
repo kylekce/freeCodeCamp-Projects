c397dfe2d03e5d8aa4e7ababab0ae3a2790685d8
Kyle Español <kylekce@gmail.com>
2024-02-02 03:18:05 +0000

    Feb 2, 2024, 11:18 AM

--- JavaScript Algorithms and Data Structures/Palindrome Checker/script.js
+++ JavaScript Algorithms and Data Structures/Palindrome Checker/script.js
@@ -2,18 +2,6 @@ const textInput = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
-checkButton.addEventListener("click", () => {
-    const text = textInput?.value || null;
-
-    if (text === null) {
-        alert("Please input a value");
-        return;
-    } else if (isPalindrome(text)) {
-        result.textContent = `${text} is a palindrome`;
-        alert(`${text} is a palindrome`);
-    }
-});
-
 function isPalindrome(text) {
     const cleanText = text.toLowerCase().replace(/[\W_]/g, "");
     const reversedText = cleanText.split("").reverse().join("");
@@ -22,3 +10,15 @@ function isPalindrome(text) {
 
     return cleanText === reversedText;
 }
+
+checkButton.addEventListener("click", () => {
+    const text = textInput?.value || null;
+
+    if (text === null) {
+        alert("Please input a value");
+        return;
+    } else if (isPalindrome(text)) {
+        result.textContent = `${text} is a palindrome`;
+        console.log(`${text} is a palindrome`);
+    }
+});
